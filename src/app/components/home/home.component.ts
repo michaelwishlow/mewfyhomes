6be7c3b2bdc684079616aeb46462ec6458f146e3
michaelwishlow <michael@pantheritconsulting.com>
2017-03-17 16:42:44 -0400

    add login function to home component/Google SignIn

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFire} from 'angularfire2';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public af:AngularFire,
+    public flashMessage:FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    this.af.auth.login();
   }
 
 }
