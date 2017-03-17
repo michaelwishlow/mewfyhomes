a9aac19ae852e2cadc92c821fcc37ab7c19c7934
michaelwishlow <michael@pantheritconsulting.com>
2017-03-17 18:22:03 -0400

    Details Page

--- src/app/components/listing/listing.component.ts
+++ src/app/components/listing/listing.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from '../../services/firebase.service';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+
+
 
 @Component({
   selector: 'app-listing',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listing.component.css']
 })
 export class ListingComponent implements OnInit {
+	id:any;
+	listing:any;
+	imageURL:any;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(
+  		private firebaseService: FirebaseService,
+  		private router:Router,
+  		private route:ActivatedRoute
+  	) { }
+
+   ngOnInit() {
+    // Get ID
+    this.id = this.route.snapshot.params['id'];
+
+    this.firebaseService.getListingDetails(this.id).subscribe(listing => {
+      this.listing = listing;
+      console.log(listing);
+
+      // @TODO - Storage Ref
+    });
   }
 
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable,FirebaseObjectObservable } from 'angularfire2';
 
 @Injectable()
 export class FirebaseService {
   listings: FirebaseListObservable<any[]>;
+  listing: FirebaseObjectObservable<any[]>;
 
   constructor(private af: AngularFire) { }
 
   getListings(){
     this.listings = this.af.database.list('/listings') as FirebaseListObservable<Listing[]>
     return this.listings;
+  }
+
+  getListingDetails(id){
+    this.listing = this.af.database.object('/listings/'+id) as FirebaseObjectObservable<Listing>
+    return this.listing;
+
   }
 
 }
